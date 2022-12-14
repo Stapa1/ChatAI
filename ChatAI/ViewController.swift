ffe013874527b6c48f6aa950179d4ed29351a304
VISHNU <97398704+Stapa1@users.noreply.github.com>
2022-12-14 15:08:47 +0530

    Initial Commit

---
+++ ChatAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAI
+//
+//  Created by Vishnu vardhan on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
